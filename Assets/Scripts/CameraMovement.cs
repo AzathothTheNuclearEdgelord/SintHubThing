48a26575e8ca3f3a48ff8f76675d7dac72329bd7
BuildTools <unconfigured@null.spigotmc.org>
2019-12-09 12:18:23 +0100

    Added camera basis

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CameraMovement : MonoBehaviour {
+    public float velocity = 10f;
+    Vector3 startPos = new Vector3(0, 15, 0);
+    Vector3 reuseVector;
+
+    Vector3 movement;
+
+    void Start() {
+        transform.position = startPos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        PlayerMovement();
+
+    }
+
+    void PlayerMovement() {
+        float distance = Time.deltaTime * velocity;
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float zoomFactor = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 verticalMovement = transform.forward * vertical;
+        Vector3 rightMovement = transform.right * horizontal;
+        Vector3 upMovement = transform.up * zoomFactor;
+        movement = (verticalMovement + rightMovement) * distance;
+        reuseVector = movement + transform.position;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            reuseVector.y += 10;
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            reuseVector.y -= 10;
+
+        reuseVector.y = Mathf.Clamp(reuseVector.y, 15, 35);
+
+        transform.position = reuseVector;
+
     }
 }
